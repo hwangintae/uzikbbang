8273ebaac75aa094a9e37a32cb61822bc7633168
intaeHwang <hwang9029@ckinform.com>
2023-07-28 13:57:30 +0900

    init

---
+++ src/main/java/com/khpl/uzikbbang/UzikbbangApplication.java
@@ -0,0 +1,13 @@
+package com.khpl.uzikbbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzikbbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UzikbbangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khpl/uzikbbang/UzikbbangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khpl.uzikbbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzikbbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
