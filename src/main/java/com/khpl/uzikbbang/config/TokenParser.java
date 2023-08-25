8a10885ae9ba657242199d3a499c4fde67b82efc
intaeHwang <hwang9029@ckinform.com>
2023-08-25 19:28:27 +0900

    토큰 파서 추가

---
+++ src/main/java/com/khpl/uzikbbang/config/TokenParser.java
@@ -0,0 +1,37 @@
+package com.khpl.uzikbbang.config;
+
+import org.springframework.stereotype.Component;
+
+import com.khpl.uzikbbang.config.data.UserSession;
+import com.khpl.uzikbbang.exception.Unauthorized;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class TokenParser {
+    
+    private final AppConfig appConfig;
+
+    public Claims parse(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(appConfig.getAuthKey())
+                    .build()
+                    .parseClaimsJws(token);
+
+            return claims.getBody();
+        } catch (Exception e) {
+            throw new Unauthorized();
+        }
+    }
+
+    public UserSession getUserSession(Claims claims) {
+        String userId = claims.getSubject();
+
+        return new UserSession(Long.parseLong(userId));
+    }
+}
