4b619e70c9e74b7a131f82ee9b7a591a8b25db1e
intaeHwang <hwang9029@ckinform.com>
2023-08-29 19:02:37 +0900

    주문 상품 조회 추가

--- src/main/java/com/khpl/uzikbbang/service/OrderService.java
+++ src/main/java/com/khpl/uzikbbang/service/OrderService.java
@@ -10,17 +10,30 @@ import com.khpl.uzikbbang.domain.CartItem;
 import com.khpl.uzikbbang.domain.UzikOrderProduct;
 import com.khpl.uzikbbang.domain.Product;
 import com.khpl.uzikbbang.domain.UzikUser;
+import com.khpl.uzikbbang.repository.OrderProductRepository;
+import com.khpl.uzikbbang.repository.UserRepository;
+import com.khpl.uzikbbang.request.Page;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final UserRepository userRepository;
+    private final OrderProductRepository orderProductRepository;
 
     @Transactional
-    public List<UzikOrderProduct> orderByProduct(UzikUser user, Product product, int cnt) {
-        return user.addOrder().orderByProduct(product, cnt);
+    public List<UzikOrderProduct> addOrderProducts(UzikUser user, Product product, int cnt) {
+        UzikUser findUser = userRepository.findById(user.getId()).get();
+        return findUser.addOrder().addOrderProducts(product, cnt);
     }
 
     @Transactional
-    public List<UzikOrderProduct> orderByCartItem(UzikUser user, List<CartItem> cartItems) {
-        return user.addOrder().orderByCartItem(cartItems);
+    public List<UzikOrderProduct> addOrderProducts(UzikUser user, List<CartItem> cartItems) {
+        return user.addOrder().addOrderProducts(cartItems);
+    }
+
+    public List<UzikOrderProduct> getOrderProducts(Page page, Long userId) {
+        return orderProductRepository.getList(page, userId);
     }
 }
