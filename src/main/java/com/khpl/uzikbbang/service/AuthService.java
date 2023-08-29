33dac23312c3ec11db91ff0f2cff76a0d4b63e85
intaeHwang <hwang9029@ckinform.com>
2023-08-29 19:01:13 +0900

    회원 조회 서비스 수정

--- src/main/java/com/khpl/uzikbbang/service/AuthService.java
+++ src/main/java/com/khpl/uzikbbang/service/AuthService.java
@@ -1,7 +1,5 @@
 package com.khpl.uzikbbang.service;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
@@ -9,10 +7,8 @@ import org.springframework.stereotype.Service;
 import com.khpl.uzikbbang.crypto.PasswordEncoder;
 import com.khpl.uzikbbang.domain.UzikUser;
 import com.khpl.uzikbbang.exception.AlreadySignUpEmailException;
-import com.khpl.uzikbbang.exception.BadCredentialsException;
 import com.khpl.uzikbbang.exception.InvalidSignInException;
 import com.khpl.uzikbbang.repository.UserRepository;
-import com.khpl.uzikbbang.request.Page;
 import com.khpl.uzikbbang.request.SignIn;
 import com.khpl.uzikbbang.request.SignUp;
 
@@ -24,19 +20,13 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public List<UzikUser> getList(Page page) {
-        return userRepository.getList(page);
-    }
-
     public UzikUser signUp(SignUp signUp) {
         String email = signUp.getEmail();
 
         // 이메일 중복 체크
-        boolean isPresent = userRepository.findByEmail(email).isPresent();
-
-        if (isPresent) {
+        userRepository.findByEmail(email).ifPresent(user -> {
             throw new AlreadySignUpEmailException();
-        }
+        });
 
         String password = passwordEncoder.encrypt(signUp.getPassword());
 
@@ -59,19 +49,5 @@ public class AuthService {
         user.addSession();
 
         return user;
-    }
-
-    public UzikUser findById(Long id) {
-        return userRepository.findById(id)
-            .orElseThrow(() -> new BadCredentialsException());
-    }
-
-    public UzikUser findByEmail(String email) {
-        return userRepository.findByEmail(email)
-            .orElseThrow(() -> new BadCredentialsException());
-    }
-
-    public void save(UzikUser user) {
-        userRepository.save(user);
     }
 }
