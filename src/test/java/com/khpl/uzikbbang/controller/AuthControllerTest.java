c7a551d8cc7be888506e6da57db4f50fb100dc47
intaeHwang <hwang9029@ckinform.com>
2023-09-01 20:04:06 +0900

    쿠키 만료 테스트 중

--- src/test/java/com/khpl/uzikbbang/controller/AuthControllerTest.java
+++ src/test/java/com/khpl/uzikbbang/controller/AuthControllerTest.java
@@ -4,21 +4,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.Duration;
+import java.util.Date;
+
 import javax.servlet.http.Cookie;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.khpl.uzikbbang.config.TokenParser;
 import com.khpl.uzikbbang.repository.UserRepository;
 import com.khpl.uzikbbang.request.SignIn;
 import com.khpl.uzikbbang.request.SignUp;
@@ -40,99 +47,142 @@ public class AuthControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TokenParser tokenParser;
+
     @BeforeEach
     void clean() {
         userRepository.deleteAll();
     }
-    
+
     @Test
     @DisplayName(value = "회원가입")
     void testSignUp() throws JsonProcessingException, Exception {
         SignUp signUp = SignUp.builder()
-            .email("hwang@hwang.com")
-            .password("1234")
-            .name("황인태")
-        .build();
+                .email("hwang@hwang.com")
+                .password("1234")
+                .name("황인태")
+                .build();
 
         mockMvc.perform(MockMvcRequestBuilders.post("/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(signUp)))
-            .andExpect(status().isOk());
+                .andExpect(status().isOk());
     }
 
     @Test
     @DisplayName(value = "로그인")
     void testSingIn() throws JsonProcessingException, Exception {
         SignUp signUp = SignUp.builder()
-            .email("hwang@hwang.com")
-            .password("1234")
-            .name("황인태")
-        .build();
+                .email("hwang@hwang.com")
+                .password("1234")
+                .name("황인태")
+                .build();
 
         authService.signUp(signUp);
 
         SignIn signIn = SignIn.builder()
-            .email("hwang@hwang.com")
-            .password("1234")
-        .build();
+                .email("hwang@hwang.com")
+                .password("1234")
+                .build();
 
         mockMvc.perform(MockMvcRequestBuilders.post("/auth/signin")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(signIn)))
-            .andExpect(status().isOk())
-            .andExpect(cookie().exists("refreshToken"))
-            .andDo(print());
+                .andExpect(status().isOk())
+                .andExpect(cookie().exists("refreshToken"))
+                .andDo(print());
     }
 
     @Test
-    @DisplayName(value = "Authorization 토큰 테스트")
+    @DisplayName(value = "Authorization accessToken 테스트")
     void testAuthorization() throws Exception {
-        String authorization = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjkzNDUzODAwLCJleHAiOjE2OTM0NTQ0MDB9.4_T23qzFXORoh43Dr99P3YCXjGVkFY2yx2RXUgrL-i4";
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/auth/foo")
-            .header("authorization", authorization))
-            .andExpect(MockMvcResultMatchers.status().isOk());
-    }
-
-    @Test
-    @DisplayName(value = "Authorization 토큰 만료 시 재발급 테스트")
-    void testNonAuthorization() throws Exception {
-        String authorization = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjkyOTU3MjUzLCJleHAiOjE2OTI5NTc4NTN9._dGdzf2LgngRleyQc_XE0mi8a43Sk1amJ8VH-w_lghI";
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/auth/foo")
-            .header("authorization", authorization))
-            .andExpect(MockMvcResultMatchers.status().isUnauthorized());
 
         SignUp signUp = SignUp.builder()
-            .email("hwang@hwang.com")
-            .password("1234")
-            .name("황인태")
-        .build();
+                .email("hwang@hwang.com")
+                .password("1234")
+                .name("황인태")
+                .build();
 
         authService.signUp(signUp);
 
         SignIn signIn = SignIn.builder()
-            .email("hwang@hwang.com")
-            .password("1234")
-        .build();
+                .email("hwang@hwang.com")
+                .password("1234")
+                .build();
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/auth/signin")
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/auth/signin")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(signIn)))
-            .andExpect(status().isOk())
-            .andExpect(cookie().exists("refreshToken"))
-            .andDo(print());
+                .andExpect(status().isOk())
+                .andExpect(cookie().exists("refreshToken"))
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        JSONObject jsonObject = new JSONObject(content);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/auth/foo")
+                .header("authorization", jsonObject.get("accessToken")))
+                .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName(value = "accessToken가 만료되었을 때, refreshToken으로 accessToken을 갱신한다.")
-    void testFoo() throws Exception {
-        Cookie cookie = new Cookie("refreshToken", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjkzNDU0MTMwLCJleHAiOjE2OTQwNTg5Mjl9.zsPxXE81SpzUGbLIpuswme9BShybZUK9U_5k1hPpXnQ");
+    @DisplayName(value = "accessToken 만료 시 재발급 테스트")
+    void testNonAuthorization() throws Exception {
+        SignUp signUp = SignUp.builder()
+                .email("hwang@hwang.com")
+                .password("1234")
+                .name("황인태")
+                .build();
+
+        authService.signUp(signUp);
+
+        SignIn signIn = SignIn.builder()
+                .email("hwang@hwang.com")
+                .password("1234")
+                .build();
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/auth/signin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signIn)))
+                .andExpect(status().isOk())
+                .andExpect(cookie().exists("refreshToken"))
+                .andReturn();
+
+        // 만료된 accessToken
+        String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjkyOTU3MjUzLCJleHAiOjE2OTI5NTc4NTN9._dGdzf2LgngRleyQc_XE0mi8a43Sk1amJ8VH-w_lghI";
         
-        mockMvc.perform(MockMvcRequestBuilders.post("/auth/refresh")
-            .cookie(cookie)
-            .header("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjkyOTU3MjUzLCJleHAiOjE2OTI5NTc4NTN9._dGdzf2LgngRleyQc_XE0mi8a43Sk1amJ8VH-w_lghI"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andDo(print());
+        Cookie cookie = result.getResponse().getCookie("refreshToken");
+
+        if (cookie == null)
+            return;
+
+        System.out.println(new Date().getTime());
+        System.out.println(cookie.getMaxAge());
+        
+        
+        // mockMvc.perform(MockMvcRequestBuilders.post("/auth/refresh")
+        //         .cookie(cookie)
+        //         .header("Authorization", accessToken))
+        //         .andExpect(status().isOk())
+        //         .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName(value = "쿠키가 만료되면 어떻게 됨?")
+    void testExpirateCookie() {
+         ResponseCookie cookie = ResponseCookie.from("test", "test")
+                .domain("localhost")
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .sameSite("Strict")
+                .maxAge(11)
+                .build();
+
+        System.out.println(cookie.getMaxAge().getSeconds());
+        System.out.println(cookie.getMaxAge());
     }
 }
