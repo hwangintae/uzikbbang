b6cd11da26246c7ca302cb1e72bd05b19d9e6a49
intaeHwang <hwang9029@ckinform.com>
2023-12-13 15:13:06 +0900

    장바구니 상품 주문 테스트 추가

--- src/test/java/com/khpl/uzikbbang/service/OrderServiceTest.java
+++ src/test/java/com/khpl/uzikbbang/service/OrderServiceTest.java
@@ -1,22 +1,24 @@
 package com.khpl.uzikbbang.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.transaction.Transactional;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.khpl.uzikbbang.domain.Product;
+import com.khpl.uzikbbang.domain.UzikOrderProduct;
 import com.khpl.uzikbbang.domain.UzikUser;
 import com.khpl.uzikbbang.repository.OrderProductRepository;
 import com.khpl.uzikbbang.repository.OrderRepository;
 import com.khpl.uzikbbang.repository.ProductRepository;
 import com.khpl.uzikbbang.repository.UserRepository;
+import com.khpl.uzikbbang.request.Page;
 import com.khpl.uzikbbang.request.ProductCreate;
 import com.khpl.uzikbbang.request.SignUp;
 
@@ -41,6 +43,12 @@ public class OrderServiceTest {
     @Autowired
     private OrderProductRepository orderProductRepository;
 
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private CartItemService cartItemService;
+
     @BeforeEach
     void clean() {
         userRepository.deleteAll();
@@ -49,14 +57,13 @@ public class OrderServiceTest {
         orderProductRepository.deleteAll();
     }
 
-
     @Test
     void testOrderByCartItem() {
 
     }
 
     @Test
-    @Transactional
+    @DisplayName("상품 바로 주문 테스트")
     void testOrderByProduct() {
         SignUp signUp = SignUp.builder()
             .name("황인태")
@@ -94,9 +101,62 @@ public class OrderServiceTest {
 
         Product product = productService.add(productCreate);
 
-        user.addOrder().orderByProduct(product, 100);
+        orderService.addOrderProducts(user, product, 100);
+
+        Page page = Page.builder().page(1).size(10).build();
+
+        List<UzikOrderProduct> orderProducts = orderService.getOrderProducts(page, user.getId());
         
-        Assertions.assertEquals(1, user.getOrders().size());
-        Assertions.assertEquals(100, user.getOrders().get(0).getOrderProducts().get(0).getCnt());
+        assertEquals(1, orderProducts.size());
+        assertEquals(100, orderProducts.get(0).getCnt());
+        assertEquals(product.getId(), orderProducts.get(0).getProduct().getId());
+    }
+
+    @Test
+    @DisplayName("장바구니 상품 주문 테스트")
+    void testOrderByProduct2() {
+        SignUp signUp = SignUp.builder()
+            .name("황인태")
+            .email("hwang@hwang.com")
+            .password("1234")
+        .build();
+
+        UzikUser user = authService.signUp(signUp);
+
+        List<String> ingredients = new ArrayList<>();
+        List<String> allergies = new ArrayList<>();
+
+        ingredients.add("우유");
+        allergies.add("우유");
+
+        ProductCreate productCreate = ProductCreate.builder()
+                    .name("우직한빵")
+                    .kind("마들렌")
+                    .addr("대흥동")
+                    .exprirationDate(5)
+                    .cost(5000)
+                    .wight(125D)
+                    .calories(1000D)
+                    .sodium(1000D)
+                    .totalCarbo(1000D)
+                    .sugars(1000D)
+                    .totalFat(1000D)
+                    .transFat(1000D)
+                    .saturatedFat(1000D)
+                    .cholesterol(1000D)
+                    .protein(1000D)
+                    .ingredients(ingredients)
+                    .allergies(allergies)
+                .build();
+
+        Product product = productService.add(productCreate);
+
+        cartItemService.addItems(user, product);
+
+
+
+
+
+        
     }
 }
