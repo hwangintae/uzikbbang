e258c4273394edd202cb4ee113612b5af025367b
intaeHwang <hwang9029@ckinform.com>
2024-02-29 16:43:03 +0900

    오류 수정

--- src/main/java/com/khpl/uzikbbang/api/service/order/OrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.khpl.uzikbbang.api.service.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.khpl.uzikbbang.domain.order.Orders;
-
-public interface OrdersRepository extends JpaRepository<Orders, Long> {
-    
-}
