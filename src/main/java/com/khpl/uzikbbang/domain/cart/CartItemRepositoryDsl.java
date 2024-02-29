f6250c2ab8f2a5e34835af461364cc5920c532b2
intaeHwang <hwang9029@ckinform.com>
2024-02-29 14:48:22 +0900

    cart 삭제

--- src/main/java/com/khpl/uzikbbang/domain/cart/CartItemRepositoryDsl.java
+++
@@ -1,7 +0,0 @@
-package com.khpl.uzikbbang.domain.cart;
-
-import java.util.Optional;
-
-public interface CartItemRepositoryDsl {
-    Optional<CartItem> getCartItem(Long userId, Long productId);
-}
